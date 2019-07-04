11057acb584d79111906a5af0ba35911b18d5d78
Sajib <mhsajib44@gmail.com>
2019-07-04 13:04:37 +0600

    context menu

--- app/src/main/java/com/tolet/sajib/androidforpractice/MainActivity.java
+++ app/src/main/java/com/tolet/sajib/androidforpractice/MainActivity.java
@@ -5,40 +5,50 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String contacts[]={"Ajay","Sachin","Sumit","Tarun","Yogesh"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contacts);
+        listView.setAdapter(adapter);
+        // Register the ListView  for Context menu
+        registerForContextMenu(listView);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.itemlist, menu);
-        return true;
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.itemlist, menu);
+        menu.setHeaderTitle("Select The Action");
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id){
-            case R.id.item1:
-                Toast.makeText(getApplicationContext(),"Item 1 Selected",Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.item2:
-                Toast.makeText(getApplicationContext(),"Item 2 Selected",Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.item3:
-                Toast.makeText(getApplicationContext(),"Item 3 Selected", Toast.LENGTH_LONG).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    public boolean onContextItemSelected(MenuItem item){
+        if(item.getItemId()==R.id.call){
+            Toast.makeText(getApplicationContext(),"calling code",Toast.LENGTH_LONG).show();
         }
+        else if(item.getItemId()==R.id.sms){
+            Toast.makeText(getApplicationContext(),"sending sms code",Toast.LENGTH_LONG).show();
+        }
+        else{
+            return false;
+        }
+        return true;
     }
 }
